47d9d3a24debef07d8fb595489299622e3158622
pedy <pedy@lednets.com>
2024-08-13 15:12:15 +0800

    feat：增加视频流展示组件示例

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import ColorPicker from './components/ColorPicker/ColorPicker.vue'
+import VideoPlayer from './components/VideoPlayer/VideoPlayer.vue'
 
 const color = ref('#ffffff')
 </script>
@@ -8,6 +9,9 @@ const color = ref('#ffffff')
 <template>
 	<div>颜色：<input type="color" v-model="color" /></div>
 	<ColorPicker v-model="color"></ColorPicker>
+	<div class="video-list">
+		<VideoPlayer v-for="i in 24" :key="i"></VideoPlayer>
+	</div>
 </template>
 
 <style lang="scss" scoped>
@@ -22,5 +26,15 @@ const color = ref('#ffffff')
 }
 .logo.vue:hover {
 	filter: drop-shadow(0 0 2em #42b883aa);
+}
+.video-list {
+	height: auto;
+	width: 1080px;
+	display: flex;
+	flex-wrap: wrap;
+	.video-player {
+		height: 100px;
+		width: 180px;
+	}
 }
 </style>
---
+++ src/components/VideoPlayer/VideoPlayer.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="video-player">
+		<canvas ref="canvasRef"></canvas>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
+import WsVideoManager from '../../utils/wsVideoManager/index'
+
+const canvasRef = ref()
+
+onMounted(() => {
+	WsVideoManager.addCanvas(canvasRef.value, 'ws://10.168.11.200:8080/record/1.mp4.live.mp4')
+})
+
+onUnmounted(() => {
+	WsVideoManager.removeCanvas(canvasRef.value)
+})
+</script>
+
+<style lang="scss" scoped>
+.video-player {
+	width: 640px;
+	height: 320px;
+	canvas {
+		height: 100%;
+		width: 100%;
+	}
+}
+</style>
