fbbc49adf0f84710d9093de62b2b3c8fb7ed81eb
pedy <pedy@lednets.com>
2024-10-30 20:22:57 +0800

    feat: throttle函数设置setTimeout为window下，避免ts报node下的setTimeout函数的错误

--- src/components/ColorPicker/utils/tools.ts
+++ src/components/ColorPicker/utils/tools.ts
@@ -1,15 +1,15 @@
 export function throttle<T extends (...args: any[]) => any>(fn: T, time: number = 200) {
-	let timer: number | null = null
-	return function (this: any, ...args: Parameters<T>) {
-		const ctx = this
+  let timer: number | null = null
+  return function (this: any, ...args: Parameters<T>) {
+    const ctx = this
 
-		if (timer) {
-			return
-		} else {
-			timer = setTimeout(() => {
-				fn.apply(ctx, args)
-				timer = null
-			}, time)
-		}
-	}
+    if (timer) {
+      return
+    } else {
+      timer = window.setTimeout(() => {
+        fn.apply(ctx, args)
+        timer = null
+      }, time)
+    }
+  }
 }
