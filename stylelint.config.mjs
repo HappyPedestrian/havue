44b07202ed6e8e043dddb08ab9c3b2f0808b7c27
pedy <pedy@lednets.com>
2025-02-20 16:10:24 +0800

    chore: 禁用stylelint的selector-class-pattern检查

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,6 +3,7 @@ export default {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order', 'stylelint-config-standard-vue/scss'],
   plugins: ['stylelint-order'],
   rules: {
-    'no-empty-source': null
+    'no-empty-source': null,
+    'selector-class-pattern': null
   }
 }
