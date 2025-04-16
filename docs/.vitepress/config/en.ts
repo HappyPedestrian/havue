fd18be7fc1ef4319e1eb9c69d5c587b1f1ac2671
pedy <pedy@lednets.com>
2025-04-16 14:40:35 +0800

    docs: 去掉vitepress英文配置中的中文配置

--- docs/.vitepress/config/en.ts
+++ docs/.vitepress/config/en.ts
@@ -14,28 +14,7 @@ export const en = defineConfig({
         base: '/',
         items: sidebarGuide()
       }
-    },
-    docFooter: {
-      prev: '上一页',
-      next: '下一页'
-    },
-    outline: {
-      label: '页面导航'
-    },
-    lastUpdated: {
-      text: '最后更新于',
-      formatOptions: {
-        dateStyle: 'short',
-        timeStyle: 'medium'
-      }
-    },
-    langMenuLabel: '多语言',
-    returnToTopLabel: '回到顶部',
-    sidebarMenuLabel: '菜单',
-    darkModeSwitchLabel: '主题',
-    lightModeSwitchTitle: '切换到浅色模式',
-    darkModeSwitchTitle: '切换到深色模式',
-    skipToContentLabel: '跳转到内容'
+    }
   }
 })
 
