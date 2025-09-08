c43beb2fd6205710289ce201f88e5f300c9ad6a0
pedy <pedy@lednets.com>
2025-09-08 14:06:05 +0800

    feat: 更新solutions和tools的vite.config.ts中getConfig的引入

--- packages/tools/vite.config.ts
+++ packages/tools/vite.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from 'vite'
-import { getConfig } from '../../internal/build/src'
+import { getConfig } from '@havue/build'
 
 export default defineConfig(({ mode }) => getConfig(mode))
